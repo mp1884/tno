b468f4018cfc7f24d0935f9581688d3b12a3b437
mp1884 <61898775+mp1884@users.noreply.github.com>
2023-12-04 12:19:29 +0700

    kal

--- 10/task.py
+++ 10/task.py
@@ -9,22 +9,47 @@ def pow_qt(p):
         return 3
     return 4
 
-MAX_POW = 200*1000
+def upd_hum(pq, hum):
+    if pq == 0:
+        return hum+3
 
-pending_hum50 = False
+    return hum-int(8*pq/4)
+
+MAX_POW = 200
 
 def solve(hum):
+    pending_hum50 = False
     hour = 0
+    cost = 0
     while hour < 24:
-        nigth = hour >= 22 or hour <= 9
+        night = hour >= 22 or hour <= 9
         pq = pow_qt(hum)
 
+        #print(f"night={night}, pq={pq}, hum={hum}, pending_hum50={pending_hum50}, "
+        #      f"hour={hour}, cost={cost}")
+
         if hum <= 39:
-            pending_hum = True
+            pending_hum50 = True
         else:
-            if pending_hum and hum >= 50:
-                    pending_hum=False
+            print(hum)
+            if pending_hum50 and hum >= 50:
+                    pending_hum50=False
 
-            if pending_hum
+        if not pending_hum50:
+            if night:
+                pq = min(pq, 2)
+        else:
+            pq = 0
 
+        hum = upd_hum(pq, hum)
+
+        cost += int(pq/4*MAX_POW)
         hour += 1
+
+        #print(f"night={night}, pq={pq}, hum={hum}, pending_hum50={pending_hum50}, "
+        #      f"hour={hour}, cost={cost}\n")
+        
+    return cost
+
+print(solve(90))
+#print(solve(int(input())))
