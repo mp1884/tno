5745372849cc000a8050cc1ef1639157990b2f6b
mp1884 <61898775+mp1884@users.noreply.github.com>
2023-12-17 22:06:11 +0700

    update test.sh for second bruteforce solution

--- 4/test.sh
+++ 4/test.sh
@@ -7,10 +7,12 @@ function run_tests {
         ti=`python gentest.py`
         ans1=`printf "%s" "$ti" | ./bf`
         ans2=`printf "%s" "$ti" | ./task`
-        if [ "$ans1" != "$ans2" ]; then
+        ans3=`printf "%s" "$ti" | ./bf2`
+        if [ "$ans1" != "$ans2" || "$ans1" != "$ans3" ]; then
             echo "$i WA"
             printf "input:\n%s\n" "$ti"
-            printf "bruteforce output:\n%s\n" "$ans1"
+            printf "bruteforce 1 output:\n%s\n" "$ans1"
+            printf "bruteforce 2 output:\n%s\n" "$ans3"
             printf "dp output:\n%s\n" "$ans2"
             exit 1
         else
